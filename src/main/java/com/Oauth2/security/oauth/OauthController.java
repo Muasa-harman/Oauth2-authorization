fbbe8d33aa464dee9cdf2d5c56beb675f09ddcfb
Muasa-harman <harman.muasa@gmail.com>
2023-11-20 22:46:14 +0300

    Oauth2.

---
+++ src/main/java/com/Oauth2/security/oauth/OauthController.java
@@ -0,0 +1,16 @@
+package com.Oauth2.security.oauth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class OauthController {
+
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello from secure endpoint");
+    }
+}
